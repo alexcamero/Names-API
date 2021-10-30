f769022be4650c21116132ff347d482794261fe8
alexcamero <alexcamero@gmail.com>
2021-10-30 12:45:07 -0500

    placeholder app

---
+++ names/__init__.py
@@ -0,0 +1,11 @@
+from markupsafe import escape
+from flask import Flask
+
+def create_app(test_config = None):
+    app = Flask(__name__, instance_relative_config = True)
+    
+    @app.route('/<name>')
+    def hello(name):
+        return f"Hello, {escape(name)}."
+
+    return app
