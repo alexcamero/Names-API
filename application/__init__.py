7d8841f8c30d61b587711a6aa7c9b53ea7e752f7
Alex Cameron <alexcamero@gmail.com>
2021-11-07 13:15:51 -0600

    starts building db-init command

--- application/__init__.py
+++ application/__init__.py
@@ -2,19 +2,20 @@ from os import environ, path
 
 from markupsafe import escape
 from flask import Flask
-from sqlalchemy import create_engine, MetaData, Table, select
+from sqlalchemy import MetaData, Table, select
 
 def create_app(test_config = None):
     app = Flask(__name__)
     if test_config == None:
         app.config.from_pyfile('config.py')
+
+    from . import models
+    models.init_app(app)
     
     @app.route('/<name>')
     def hello(name):
         reply = [f"Hello, {escape(name)}."]
-        basedir = path.abspath(path.dirname(__file__))
-        path_to_ca = path.join(basedir, "DigiCertGlobalRootCA.crt.pem")
-        engine = create_engine(f"mysql+pymysql://{app.config['DATABASE_USER']}:{app.config['DATABASE_PASSWORD']}@{app.config['DATABASE_HOST']}/{app.config['DATABASE_NAME']}?ssl_ca={path_to_ca}")
+        engine = models.get_engine()
         metadata_obj = MetaData()
         best_names = Table("best_names", metadata_obj, autoload_with = engine)
         stmt = select(best_names).where(best_names.c.name == name)
